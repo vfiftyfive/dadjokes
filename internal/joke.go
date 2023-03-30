e95aa69a79989d829dc4280170932192bcdde4fb
vfiftyfive <vfiftyfive@gmail.com>
2023-03-30 17:14:20 +0100

    add joke server

---
+++ internal/joke.go
@@ -0,0 +1,38 @@
+package internal
+
+import (
+	"context"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+type Joke struct {
+	ID   string
+	Text string
+}
+
+func generateJoke(client *openai.Client) (string, error) {
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Tell me dad joke",
+				},
+			},
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	if len(resp.Choices) == 0 {
+		return "", err
+	}
+	joke := resp.Choices[0].Message.Content
+
+	return joke, nil
+
+}
