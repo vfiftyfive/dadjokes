25266cffca211ca3d6f300d2c96987c22c2cc24b
vfiftyfive <vfiftyfive@gmail.com>
2024-04-18 00:23:22 +0100

    add aws connection code

---
+++ internal/aws/aws.go
@@ -0,0 +1,21 @@
+package aws
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+// DynamoDBClient creates and returns a DynamoDB client
+func DynamoDBClient() *dynamodb.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-west-2"), // Fallback region
+	)
+	if err != nil {
+		log.Fatalf("Unable to load SDK config, %v", err)
+	}
+
+	return dynamodb.NewFromConfig(cfg)
+}
